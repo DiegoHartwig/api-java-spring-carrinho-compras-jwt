dc15230b136b39e7d64d71ecc67493184f934ffd
Diego Hartwig <hartwig.diego@gmail.com>
2018-10-03 17:40:40 -0300

    Criacao do projeto Spring

---
+++ src/main/java/br/net/hartwig/apicarrinhocompras/ApicarrinhocomprasApplication.java
@@ -0,0 +1,12 @@
+package br.net.hartwig.apicarrinhocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicarrinhocomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicarrinhocomprasApplication.class, args);
+	}
+}

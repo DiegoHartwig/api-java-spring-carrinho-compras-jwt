e25be189ede3f851e956662a88b8ef576df498cd
Diego Hartwig <hartwig.diego@gmail.com>
2018-10-22 21:15:16 -0300

    Implementação das Classes Cliente, Endereco, Associacoes, mapeamento objeto relacional e Repository

--- src/main/java/br/net/hartwig/apicarrinhocompras/ApicarrinhocomprasApplication.java
+++ src/main/java/br/net/hartwig/apicarrinhocompras/ApicarrinhocomprasApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.net.hartwig.apicarrinhocompras.domain.Categoria;
 import br.net.hartwig.apicarrinhocompras.domain.Cidade;
+import br.net.hartwig.apicarrinhocompras.domain.Cliente;
+import br.net.hartwig.apicarrinhocompras.domain.Endereco;
 import br.net.hartwig.apicarrinhocompras.domain.Estado;
 import br.net.hartwig.apicarrinhocompras.domain.Produto;
+import br.net.hartwig.apicarrinhocompras.domain.enums.TipoCliente;
 import br.net.hartwig.apicarrinhocompras.repositories.CategoriaRepository;
 import br.net.hartwig.apicarrinhocompras.repositories.CidadeRepository;
+import br.net.hartwig.apicarrinhocompras.repositories.ClienteRepository;
+import br.net.hartwig.apicarrinhocompras.repositories.EnderecoRepository;
 import br.net.hartwig.apicarrinhocompras.repositories.EstadoRepository;
 import br.net.hartwig.apicarrinhocompras.repositories.ProdutoRepository;
 
@@ -21,15 +26,21 @@ public class ApicarrinhocomprasApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApicarrinhocomprasApplication.class, args);
@@ -41,37 +52,53 @@ public class ApicarrinhocomprasApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Pneus");
 		Categoria categoria2 = new Categoria(null, "Capacetes");
 		Categoria categoria3 = new Categoria(null, "Acessórios");
-		
+
 		Produto produto1 = new Produto(null, "Pneu Metzeler", 899.00);
 		Produto produto2 = new Produto(null, "Capacete Old School", 150.00);
 		Produto produto3 = new Produto(null, "Comando Avançado VT 600", 650.00);
-		
+
 		categoria1.getProdutos().addAll(Arrays.asList(produto1));
 		categoria2.getProdutos().addAll(Arrays.asList(produto2));
 		categoria3.getProdutos().addAll(Arrays.asList(produto3));
-		
+
 		produto1.getCategorias().addAll(Arrays.asList(categoria1));
 		produto2.getCategorias().addAll(Arrays.asList(categoria2));
-		produto3.getCategorias().addAll(Arrays.asList(categoria3));				
+		produto3.getCategorias().addAll(Arrays.asList(categoria3));
 
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3));
-		
+
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
-		
+
 		Estado estado1 = new Estado(null, "Paraná");
 		Estado estado2 = new Estado(null, "Santa Catarina");
 		Estado estado3 = new Estado(null, "Rio Grande do Sul");
-		
+
 		Cidade cidade1 = new Cidade(null, "Curitiba", estado1);
 		Cidade cidade2 = new Cidade(null, "Florianopolis", estado2);
 		Cidade cidade3 = new Cidade(null, "Porto Alegre", estado3);
-		
+
 		estado1.getCidades().addAll(Arrays.asList(cidade1));
 		estado2.getCidades().addAll(Arrays.asList(cidade2));
 		estado3.getCidades().addAll(Arrays.asList(cidade3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(estado1, estado2, estado3));
 		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+
+		Cliente cliente1 = new Cliente(null, "Diego Hartwig", "hartwig.diego@gmail.com", "12312311231",
+				TipoCliente.PESSOAFISICA);
+		cliente1.getTelefones().addAll(Arrays.asList("4130123344", "41996505412"));
+
+		Endereco endereco1 = new Endereco(null, "Av Marechal Floriano Peixoto", "1", "", "Centro", "12312333", cliente1,
+				cidade1);
+
+		Endereco endereco2 = new Endereco(null, "Rua Isaac Ferreira da Cruz", "2", "", "Sitio Cercado", "1234423234",
+				cliente1, cidade1);
+
+		cliente1.getEnderecos().addAll(Arrays.asList(endereco1, endereco2));
+
+		clienteRepository.saveAll(Arrays.asList(cliente1));
+
+		enderecoRepository.saveAll(Arrays.asList(endereco1, endereco2));
 
 	}
 }
---
+++ src/main/java/br/net/hartwig/apicarrinhocompras/domain/Cliente.java
@@ -0,0 +1,132 @@
+package br.net.hartwig.apicarrinhocompras.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import br.net.hartwig.apicarrinhocompras.domain.enums.TipoCliente;
+
+@Entity
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfCnpj;
+	private Integer tipoCliente;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+
+	public Cliente() {
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfCnpj, TipoCliente tipoCliente) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfCnpj = cpfCnpj;
+		this.tipoCliente = tipoCliente.getCodigo();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public TipoCliente getTipoCliente() {
+		return TipoCliente.toEnum(tipoCliente);
+	}
+
+	public void setTipoCliente(TipoCliente tipoCliente) {
+		this.tipoCliente = tipoCliente.getCodigo();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}

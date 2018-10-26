3d4572547576ce992ba9e2039512741bcf59c779
Diego Hartwig <hartwig.diego@gmail.com>
2018-10-25 22:02:29 -0300

    metodo para inserir categoria

--- src/main/java/br/net/hartwig/apicarrinhocompras/resources/CategoriaResource.java
+++ src/main/java/br/net/hartwig/apicarrinhocompras/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package br.net.hartwig.apicarrinhocompras.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.net.hartwig.apicarrinhocompras.domain.Categoria;
 import br.net.hartwig.apicarrinhocompras.services.CategoriaService;
@@ -24,6 +28,15 @@ public class CategoriaResource {
 
 		return ResponseEntity.ok().body(categoria);
 
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/br/net/hartwig/apicarrinhocompras/services/CategoriaService.java
+++ src/main/java/br/net/hartwig/apicarrinhocompras/services/CategoriaService.java
@@ -16,11 +16,16 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
-		
+
 		Optional<Categoria> obj = repo.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria categoria) {
+		categoria.setId(null);
+		return repo.save(categoria);
 	}
 
 }
